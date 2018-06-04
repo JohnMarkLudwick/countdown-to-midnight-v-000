dd023852eab346c48d5a08fa5f0e4f7205ec75a6
JohnMark <johnmarkludwick@gmail.com>
2018-06-04 14:19:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -14,14 +14,10 @@ def countdown(number)
 end
 
 
-def countown_with_sleep(5)
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countown_with_sleep(number)
     sleep 1
   end
-  # puts "HAPPY NEW YEAR!"
-  "HAPPY NEW YEAR!"
+ 
 
 
 end
